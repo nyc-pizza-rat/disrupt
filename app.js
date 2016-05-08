9bb57a5ed80a5ad63e0542ea1d4174597bbc2fcc
Max Peterson <petersonmaxr@gmail.com>
2016-05-07 20:06:53 -0400

    Adds minimal routes for health codes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/user');
+var healthCodes = require('./routes/health-codes');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/health-codes', healthCodes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
