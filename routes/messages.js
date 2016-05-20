98842ddbbdfeee66b234fa7ae93528de79975332
Max Peterson <petersonmaxr@gmail.com>
2016-05-20 18:15:38 -0400

    consolidates api-facing and regular modules into one location

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const responses = require('../more_modules/responses');
+const responses = require('../modules/responses');
 
 router.post('/', function(req, res) {
   console.log(req.body);
